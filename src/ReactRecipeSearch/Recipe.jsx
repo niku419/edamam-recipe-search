ac37481ffec1e1953cdf131febcd8d351abde5cd
Niku_419 <2019198@iiitdmj.ac.in>
2021-06-26 13:50:25 +0530

    :dart: added button to Recipe card

--- src/ReactRecipeSearch/Recipe.jsx
+++ src/ReactRecipeSearch/Recipe.jsx
@@ -8,36 +8,41 @@ export default function Recipe() {
   const {index} = useParams()
   
   return (
-    <Container className="row">
-      <Container className="col pt-5 d-flex align-items-center justify-content-center">
-        <Card className="mb-3" style={{maxWidth: "540px", width: "18rem"}} >
-          <Card.Img className="img-fluid" style={{maxWidth: "100%", height: "auto"}} src={recipeResults[index].recipe.image} alt="..."/>
-          <Card.Title className="blockquote pt-2" style={{textAlign: "center"}}>
-          {(recipeResults[index].recipe.label)}
-          <footer className="blockquote-footer">
-            <small className="text-muted">
-              <cite title="Source Title">{`${recipeResults[index].recipe.cuisineType[0]} ${recipeResults[index].recipe.mealType[0]}`}</cite>
-            </small>
-          </footer>
-        </Card.Title>
-        </Card>
+    <>
+      <Container className="row">
+        <Container className="col pt-5 d-flex align-items-center justify-content-center">
+          <Card className="mb-3" style={{maxWidth: "540px", width: "18rem"}} >
+            <Card.Img className="img-fluid" style={{maxWidth: "100%", height: "auto"}} src={recipeResults[index].recipe.image} alt="..."/>
+            <Card.Title className="blockquote pt-2" style={{textAlign: "center"}}>
+            {(recipeResults[index].recipe.label)}
+            <footer className="blockquote-footer">
+              <small className="text-muted">
+                <cite title="Source Title">{`${recipeResults[index].recipe.cuisineType[0]} ${recipeResults[index].recipe.mealType[0]}`}</cite>
+              </small>
+            </footer>
+          </Card.Title>
+          </Card>
+        </Container>
+        <Container className="col pt-5 d-flex align-items-center justify-content-center">
+          <Card.Body>
+            <ul>
+              <li><strong>Number of Serves</strong>: {recipeResults[index].recipe.yield}</li>
+              <li><strong>Total Weight</strong>: {recipeResults[index].recipe.totalWeight}</li>
+              <li><strong>Total Calories</strong>: {recipeResults[index].recipe.calories}</li>
+            </ul>
+            <ul>
+              {recipeResults[index].recipe.ingredients.map((ingredient, index) =>(
+                <li key={index}>
+                  <strong>{ingredient.text}</strong>: {ingredient.weight}g
+                </li>
+              ))}
+            </ul>
+          </Card.Body>
+        </Container>
       </Container>
-      <Container className="col pt-5 d-flex align-items-center justify-content-center">
-        <Card.Body>
-          <ul>
-            <li><strong>Number of Serves</strong>: {recipeResults[index].recipe.yield}</li>
-            <li><strong>Total Weight</strong>: {recipeResults[index].recipe.totalWeight}</li>
-            <li><strong>Total Calories</strong>: {recipeResults[index].recipe.calories}</li>
-          </ul>
-          <ul>
-            {recipeResults[index].recipe.ingredients.map((ingredient, index) =>(
-              <li key={index}>
-                <strong>{ingredient.text}</strong>: {ingredient.weight}g
-              </li>
-            ))}
-          </ul>
-        </Card.Body>
-      </Container>
-    </Container>
+      <div className="d-flex justify-content-between py-3 pl-5" >
+        <a class="btn btn-outline-secondary" href="/" role="button">Back to Search!!</a>
+      </div>
+    </>
   )
 }
